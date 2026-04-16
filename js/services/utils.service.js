526be955959297e7882049c495493c30fdb58ca6
Davian <daniacev20@gmail.com>
2026-04-16 10:37:14 -0400

    wip: added the Cookie class with methods to get, set and delete cookies to document.cookie.

--- js/services/utils.service.js
+++ js/services/utils.service.js
@@ -1,8 +1,42 @@
 // utils.service.js
 
 // utils objects
-const regexValidators = {
+export const regexValidators = {
 	PHONE: /^\(?\d{3}\)?(\s|\.|-)?\d{3}(\s|\.|-)?\d{4}$/,
 	EMAIL: /^(?!.*\.\.)[a-zA-z0-9._%+-]+@[a-zA-z0-9.-]+\.[a-zA-Z]{2,}$/,
 	DATE: null // wip
+}
+
+export class Cookie {
+	static setCookie(name, value, exp = 2) {
+		const parsedValue = typeof value === 'object' ?
+			JSON.stringify(value) : value.toString();
+		const currentDate = new Date();
+		const hours = 1000 * 60 * 60 * exp;
+		const expDateStr = new Date(currentDate.getTime() + hours).toUTCString();
+
+		document.cookie = `${name}=${parsedValue};expires=${expDateStr};path=/;`;
+
+		return document.cookie.split(";").some(c => c.startsWith(`${name}=`));
+	}
+
+	static getCookie(name) {
+		const cookies = document.cookie.split(";").map(p => p.trim());
+
+		for (const cookie of cookies) {
+			if (cookie.includes(name))
+				return cookie.substring(cookie.indexOf("=") + 1);
+		}
+
+		return "";
+	}
+
+	static deleteCookie(name) {
+		if (this.getCookie(name)) {
+			this.setCookie(name, "", -1)
+			return true;
+		}
+
+		return false;
+	}
 }
